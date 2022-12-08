7d541847ed05cd8b7e0163278944afcf9be09121
sriram14a <sriramjs07@gmail.com>
2022-12-08 10:23:49 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Dislike />
+     
     </div>
   );
 }
 
+function Dislike(){
+  const [dislike,setDislike] = useState(0);
+
+  return(
+    <div className="App">
+    <button onClick={()=>{
+      setDislike(dislike + 1);
+    }}>👎{dislike}</button>
+  </div>
+  );
+}
 export default App;
